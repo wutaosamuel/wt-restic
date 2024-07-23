ae16f16a9b4ac721265ee79c0db54e7afb6a6b09
Tao Wu <Tao.Wu@microchip.com>
2024-07-23 16:38:23 +0800

    first version backup config for mobile win11

--- windows/win11/mobile_main_restic.sh
+++ windows/win11/mobile_main_restic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# restic_test
-#restic -r /root/tmp/restic_test --verbose backup --files-from /root/restic/restic_includes.txt --exclude-file /root/restic/restic_excludes.txt
-
 # restic
 resticDir="$(dirname $0)"
-envFile="$resticDir"/restic_env.txt
-source $envFile
+#envFile="$resticDir"/restic_env.txt
+#source $envFile
 
-restic -r b2:greencloud:restic_backup --verbose backup --files-from /root/restic/restic_includes.txt --exclude-file /root/restic/restic_excludes.txt
+includeFileList="$resticDir"/mobile_main_restic_includes.txt
+excludeFileList="$resticDir"/mobile_main_restic_excludes.txt
+
+restic -r C:/D/storage-encrypt/restic --verbose backup --files-from "$includeFileList" --exclude-file "$excludeFileList"
